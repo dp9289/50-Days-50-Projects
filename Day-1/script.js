c07726da0bad5b024fc7f324c11cb62eb6a3c267
Deep <pdeepr4321@gmail.com>
2021-05-25 13:49:35 +0530

    initial commit

---
+++ Day-1/script.js
@@ -0,0 +1,14 @@
+let cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        removeClassActive();
+        card.classList.add('active');
+    })
+});
+
+function removeClassActive() {
+    cards.forEach(card => {
+        card.classList.remove('active');
+    })
+}
