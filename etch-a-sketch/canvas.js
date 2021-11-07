07fc54e30f5ddd5987e8f8b26133d552aba9a301
Deep <pdeepr4321@gmail.com>
2021-11-07 18:55:40 +0530

    add rainbow stroke

--- etch-a-sketch/canvas.js
+++ etch-a-sketch/canvas.js
@@ -2,12 +2,12 @@
 const canvas = document.getElementById("etch-a-sketch");
 const btn = document.getElementById("btn");
 const ctx = canvas.getContext("2d");
-
+let hue = 0;
 //setup canvas
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 ctx.lineWidth = 10;
-ctx.strokeStyle = "#fff";
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
 //using destructuring to create widht and height varibles from canvas properties
 const { width, height } = canvas;
@@ -22,6 +22,8 @@ ctx.lineTo(x, y); //create a line at this position in the canvas
 ctx.stroke(); //this actually draws the line at given position in the canvas
 
 function draw({ key }) {
+  hue += 10;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   ctx.moveTo(x, y);
   switch (key) {
