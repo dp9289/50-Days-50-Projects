03b569b0c48ac63439bf1ab3750b9f2e0d66402a
Deep <pdeepr4321@gmail.com>
2021-11-21 15:03:58 +0530

    added funky function

--- Text Generator/script.js
+++ Text Generator/script.js
@@ -2,6 +2,8 @@ const text = document.querySelector("#text");
 const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
 const output = document.querySelector("#output");
 
+const funkyLetters = {
+    '-': '₋', '!': 'ᵎ', '?': 'ˀ', '(': '⁽', ')': '₎', '+': '⁺', '=': '₌', '0': '⁰', '1': '₁', '2': '²', '4': '₄', '5': '₅', '6': '₆', '7': '⁷', '8': '⁸', '9': '⁹', a: 'ᵃ', A: 'ᴬ', B: 'ᴮ', b: 'ᵦ', C: '𝒸', d: 'ᵈ', D: 'ᴰ', e: 'ₑ', E: 'ᴱ', f: '𝒻', F: 'ᶠ', g: 'ᵍ', G: 'ᴳ', h: 'ʰ', H: 'ₕ', I: 'ᵢ', i: 'ᵢ', j: 'ʲ', J: 'ᴶ', K: 'ₖ', k: 'ₖ', l: 'ˡ', L: 'ᴸ', m: 'ᵐ', M: 'ₘ', n: 'ₙ', N: 'ᴺ', o: 'ᵒ', O: 'ᴼ', p: 'ᵖ', P: 'ᴾ', Q: 'ᵠ', q: 'ᑫ', r: 'ʳ', R: 'ᵣ', S: 'ˢ', s: 'ˢ', t: 'ᵗ', T: 'ₜ', u: 'ᵘ', U: 'ᵤ', v: 'ᵛ', V: 'ᵥ', w: '𝓌', W: 'ʷ', x: 'ˣ', X: 'ˣ', y: 'y', Y: 'Y', z: '𝓏', Z: 'ᶻ' };
 
 function sarcastic(letter, index) {
   if (index % 2 == 0) {
@@ -9,11 +11,18 @@ function sarcastic(letter, index) {
   }
   return letter.toUpperCase();
 }
-function funky() {}
+function funky(letter) {
+    let funky = funkyLetters[letter];
+    if(funky) return funky;
+    funky = funkyLetters[letter.toLowerCase()];
+    if(funky) return funky;
+    return letter;
+}
+
 function unable() {}
 function transformText(text) {
-  const filter = filterInputs.find((inputChecked) => inputChecked.checked);
-  const result = Array.from(text).map(sarcastic);
+  const checkedInput = filterInputs.find((inputChecked) => inputChecked.checked).value;
+  const result = Array.from(text).map(checkedInput);
   output.textContent = result.join("");
 }
 
