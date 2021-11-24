cfa273a5a306dfd5f111aeca5b3ca33d26cb1ada
Deep <pdeepr4321@gmail.com>
2021-11-24 20:28:18 +0530

    add function to display items

--- Shopping List/script.js
+++ Shopping List/script.js
@@ -13,7 +13,20 @@ function handleSubmit(e) {
   };
   items.push(item);
   //   e.currentTarget.item.value = '';
+  displayItem();
   e.target.reset();
 }
 
+function displayItem() {
+  const html = items
+    .map(
+      (item) =>
+        `<li>
+      <span>${item.name}</span>
+      <button>&times;</button>
+    </li>`
+    )
+    .join("");
+  list.innerHTML = html;
+}
 form.addEventListener("submit", handleSubmit);
