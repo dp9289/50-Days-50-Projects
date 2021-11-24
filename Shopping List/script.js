811e11e444e1899c054c637fadece0a7537172dd
Deep <pdeepr4321@gmail.com>
2021-11-24 19:58:56 +0530

    created items array of object

--- Shopping List/script.js
+++ Shopping List/script.js
@@ -1,16 +1,17 @@
 const form = document.querySelector("#form");
 const list = document.querySelector(".list");
 
+const items = [];
+
 function handleSubmit(e) {
-    e.preventDefault();
-  const item = e.currentTarget.item.value;
-  const html = `
-    <li>
-    <span>${item}</span>
-    <button>&times;</button>
-    </li>
-  `;
-  list.innerHTML += html;
+  e.preventDefault();
+  const name = e.currentTarget.item.value;
+  const item = {
+    name: name,
+    id: Date.now(),
+    complete: false,
+  };
+  items.push(item);
   //   e.currentTarget.item.value = '';
   e.target.reset();
 }
