2152bb6bc2f7a049b6ff804df294ed257a45e416
Deep <pdeepr4321@gmail.com>
2022-01-16 16:09:47 +0530

    added event to remove transition

--- Drum Kit/script.js
+++ Drum Kit/script.js
@@ -1,14 +1,26 @@
-
 function playMusic(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
-    if(!audio) return;
+  if (!audio) return;
 
-    key.classList.add("playing");
-    audio.currentTime = 0;
-    audio.play();
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
 }
 
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
 
-window.addEventListener("keydown", playMusic)
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playMusic);
+
+/*
+ Things i learned from this project:
+ 1. Transitionend event => It is fired when CSS transition has completed.
+ 2. currentTime = 0 to reset the audio play time
+*/
